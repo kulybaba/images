c55ad474b238073626e203efdd2ff46b3b2a1b1f
Petro Kulybaba <ahurtep@gmail.com>
2018-08-08 12:45:43 +0300

    Module user

--- frontend/modules/user/views/default/login.php
+++ frontend/modules/user/views/default/login.php
@@ -38,7 +38,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="col-lg-5">
             <h3>Login with Google</h3>
             <?= yii\authclient\widgets\AuthChoice::widget([
-                'baseAuthUrl' => ['site/auth'],
+                'baseAuthUrl' => ['/user/default/auth'],
                 'popupMode' => false,
             ]) ?>
         </div>
