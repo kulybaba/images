18ba3a0a1e095811652f4568e20f385252186ad1
Petro Kulybaba <ahurtep@gmail.com>
2018-08-09 14:00:52 +0300

    Profile page improvements

--- frontend/modules/user/views/profile/view.php
+++ frontend/modules/user/views/profile/view.php
@@ -1,2 +1,11 @@
-<h1>Hello!</h1>
-<p><?php echo $user->username; ?></p>
+<?php 
+/* @var $this yii\web\View */
+/* @var $user frontend\models\User */
+
+use yii\helpers\Html;
+use yii\helpers\HtmlPurifier;
+?>
+
+<h1><?php echo Html::encode($user->username); ?></h1>
+<p><?php echo HtmlPurifier::process($user->about); ?></p>
+<hr>
